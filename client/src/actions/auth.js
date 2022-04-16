eb996dce0535a139575ef6ba7049a3bd2bf6f8cd
Arunkumar2611 <arunkumar2611@github.com>
2022-04-16 19:10:24 +0530

    setting login and signup from the backend side

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,6 +5,10 @@ export const signin = (formData, navigate) => async (dispatch) => {
     try {
         /* Log in the user */
         /* we use navigate bcz after login we will navigate to the home page */
+        const { data } = await api.signIn(formData);
+        
+        dispatch({type: AUTH, data});
+
         navigate('/')
     } catch (error) {
         console.log(error.message);
@@ -16,6 +20,10 @@ export const signup = (formData, navigate) => async (dispatch) => {
     try {
         /* Sign up in the user */
         /* we use navigate bcz after login we will navigate to the home page */
+        const { data } = await api.signUp(formData);
+
+        dispatch({ type: AUTH, data });
+
         navigate('/')
     } catch (error) {
         console.log(error.message);
