3679eeb8328a96a64d850ab31d9f3e64f11a083c
Arunkumar2611 <arunkumar2611@github.com>
2022-04-28 11:22:10 +0530

    adding code for search post and search tag and also adding code for navigation

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,35 +1,115 @@
 import React, { useState, useEffect } from "react";
-import { Container, Grid, Grow } from "@material-ui/core";
+import {
+  Container,
+  Grid,
+  Grow,
+  Paper,
+  AppBar,
+  TextField,
+  Button,
+} from "@material-ui/core";
 import { useDispatch } from "react-redux";
-import { getPosts } from "../../actions/posts";
+import { getPosts, getPostsBySearch } from "../../actions/posts";
 import Form from "../Form/Form";
 import Posts from "../Posts/Posts";
 import useStyle from "./styles";
+import Paginate from "../Pagination/Pagination";
+import { useNavigate, useLocation } from "react-router-dom";
+import ChipInput from 'material-ui-chip-input';
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
 const Home = () => {
   const [currentId, setCurrentId] = useState(null);
   const classes = useStyle();
   const dispatch = useDispatch();
+  const query = useQuery();
+  const history = useNavigate();
+  const page = query.get("page") || 1;
+  const searchQuery = query.get("searchQuery");
 
   useEffect(() => {
     dispatch(getPosts());
-  }, [dispatch]);
+  }, [currentId, dispatch]);
+
+  const [search, setSearch] = useState("");
+  const [tags, setTags] = useState([]);
+
+  const searchPost = () => {
+    if (search.trim() || tags) {
+      dispatch(getPostsBySearch({ search, tags: tags.join(",") }));
+      history(
+        `/posts/search?searchQuery=${search || "none"}&tags=${tags.join(",")}`
+      );
+    } else {
+      history("/");
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.keyCode === 13) {
+      searchPost();
+    }
+  };
+
+  const handleAddChip = (tag) => setTags([...tags, tag]);
+
+  const handleDeleteChip = (chipToDelete) =>
+    setTags(tags.filter((tag) => tag !== chipToDelete));
 
   return (
     <Grow in>
-      <Container>
+      <Container maxWidth="xl">
         <Grid
-          className={classes.mainContainer}
           container
           justify="space-between"
           alignItems="stretch"
-          spacing="3"
+          spacing={3}
+          className={classes.gridContainer}
         >
-          <Grid item xs={12} sm={7}>
+          <Grid item xs={12} sm={6} md={9}>
             <Posts setCurrentId={setCurrentId} />
           </Grid>
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={12} sm={6} md={3}>
+            <AppBar
+              className={classes.appBarSearch}
+              position="static"
+              color="inherit"
+            >
+              <TextField
+                onKeyDown={handleKeyPress}
+                name="search"
+                variant="outlined"
+                label="Search Memories"
+                fullWidth
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <ChipInput
+                style={{ margin: "10px 0" }}
+                value={tags}
+                onAdd={(chip) => handleAddChip(chip)}
+                onDelete={(chip) => handleDeleteChip(chip)}
+                label="Search Tags"
+                variant="outlined"
+              />
+              <Button
+                onClick={searchPost}
+                className={classes.searchButton}
+                variant="contained"
+                color="primary"
+              >
+                Search
+              </Button>
+            </AppBar>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
+            {!searchQuery && !tags.length && (
+              <Paper className={classes.pagination} elevation={6}>
+                <Paginate page={page} />
+              </Paper>
+            )}
           </Grid>
         </Grid>
       </Container>
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
+import { getPostsBySearch, getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
 const router = express.Router();
 import auth from '../middleware/auth.js';
 
 router.get('/', getPosts);
+router.get('/search', getPostsBySearch);
 router.post('/', auth, createPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
